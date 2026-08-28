267f240d556195b3c08da449e4f87c0d5d1c77d7
rock <rocksocar@gmail.com>
2026-08-28 19:09:27 +0200

    added canonball shooting

--- papir-stridsvogner/kanon.gd
+++ papir-stridsvogner/kanon.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
-@onready var point_direction = Vector2.RIGHT
+var point_direction = Vector2.UP
+var kanonball = load("res://kanonball.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,12 +13,25 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("shoot_up"):
 		point_direction = Vector2.UP
+		shoot()
 	if Input.is_action_just_pressed("shoot_left"):
 		point_direction = Vector2.LEFT
+		shoot()
 	if Input.is_action_just_pressed("shoot_right"):
 		point_direction = Vector2.RIGHT
+		shoot()
 	if Input.is_action_just_pressed("shoot_down"):
 		point_direction = Vector2.DOWN
+		shoot()
 		
 	self.global_rotation = point_direction.angle() + deg_to_rad(90.0)
 	pass
+
+func shoot():
+	var kanonball_instance = kanonball.instantiate()
+	kanonball_instance.set_retning(point_direction)
+	kanonball_instance.transform.origin = self.global_transform.origin
+	kanonball_instance.velocity += get_parent().velocity / 5
+	get_parent().add_sibling(kanonball_instance)
+	
+	
--- papir-stridsvogner/spiller.gd
+++ papir-stridsvogner/spiller.gd
@@ -5,8 +5,8 @@ const SPEED = 300.0
 
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	#Input, generates a float from -1.0 to 1.0 for up and down directions
 	var x_direction := Input.get_axis("move_left", "move_right")
 	var y_direction := Input.get_axis("move_up", "move_down")
 	
@@ -15,14 +15,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = x_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	if y_direction:
 		velocity.y = y_direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-	
+		
+	#if you let go of movement keys, the belts won't snap to facing right when the tank stops moving
 	if (x_direction == 0) and (y_direction == 0):
 		pass
 	else: 
-		self.global_rotation = velocity.angle() + deg_to_rad(90.0)
+		self.global_rotation = velocity.angle()
 
 	move_and_slide()
