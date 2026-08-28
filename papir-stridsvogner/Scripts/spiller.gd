c5803a57613aad79e4adc0b1666c96ba8f8bc676
nuclearmayhem1 <53118215+nuclearmayhem1@users.noreply.github.com>
2026-08-28 17:05:33 +0200

    Organized

--- papir-stridsvogner/Scripts/spiller.gd
+++ papir-stridsvogner/Scripts/spiller.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var x_direction := Input.get_axis("move_left", "move_right")
 	var y_direction := Input.get_axis("move_up", "move_down")
 	
