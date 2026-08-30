f2e53fbe45006bc13580f571164387b2c21c5349
rock <rocksocar@gmail.com>
2026-08-30 18:49:15 +0200

    bandit add, polish of other stuff

---
+++ papir-stridsvogner/Scripts/banditt.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var spiller: Node2D
+var speed = 50
+
+func _ready():
+	var spiller_liste = get_tree().get_nodes_in_group("spiller")
+	assert(spiller_liste.size() == 1)
+	
+	spiller = spiller_liste[0]
+
+
+func _physics_process(delta: float) -> void:
+	#hvis spilleren ikke er tilgjengelig stopper det koden å fortsett
+	if spiller == null: 
+		return
+	
+	#håndterer bevegelse (bestemmer retning)
+	var direction: Vector2 = (spiller.global_position - global_position).normalized()
+	velocity = direction * speed #retning + fart = hastighet
+	self.global_rotation = velocity.angle()
+	move_and_slide()
--- papir-stridsvogner/Scripts/kanon.gd
+++ papir-stridsvogner/Scripts/kanon.gd
@@ -3,14 +3,9 @@ extends Sprite2D
 var firing_speed = 500
 var firing_direction = Vector2.UP
 var kanonball = preload("res://Scenes/kanonball.tscn")
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	
 	if Input.is_action_just_pressed("shoot_up"):
 		firing_direction = Vector2.UP
@@ -26,7 +21,7 @@ func _process(delta: float) -> void:
 		shoot()
 		
 	self.global_rotation = firing_direction.angle() + deg_to_rad(90.0)
-	
+
 
 func shoot():
 	# Lag en ny kanonball
@@ -37,5 +32,3 @@ func shoot():
 	kanonball_instance.velocity = firing_direction * firing_speed
 	# Plasser den i verden som "søsken" til spilleren, slik at den ikke henger etter spilleren
 	get_parent().add_sibling(kanonball_instance)
-	
-	
